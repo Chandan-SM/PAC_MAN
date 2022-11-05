bb353189e1e46162e6739c670985de03688fbaae
Chandan SM <113344692+Chandan-Gowda-Kalvium@users.noreply.github.com>
2022-11-05 22:21:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const username = document.getElementById("username-field")
+const nickname = document.getElementById("text-field")
+
 let audio = new Audio("./images/main.mp3")
 setTimeout( () => {
     audio.play();
     audio.loop = true;
 }, 10);
+
+const start=document.getElementById("start_btn")
+start.onclick = () => {
+    location.href="./game.html"
+    storename()
+}
+function storename() {
+    localStorage.setItem('userid',username.value)
+    localStorage.setItem('user',nickname.value)
+}
